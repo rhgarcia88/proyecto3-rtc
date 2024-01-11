e25aa8b724d2bdf5795bac1804a2734ed8cf7055
rhgarcia88 <rhgarguer@gmail.com>
2024-01-11 19:07:45 +0100

    logoroute3

--- components/header/header.js
+++ components/header/header.js
@@ -2,7 +2,7 @@ import "./headerStyles.css";
 
 export const generateHeader = () => {
   return `<header>
-<div class="logo-div"><img src="../assets/logo-big.png" alt="logo"> </div>
+<div class="logo-div"><img src="/logo-big.png" alt="logo"> </div>
   <div id="input-container"><input type="text" placeholder="🔎 ¿Qué quieres ver hoy?" id="search-input"><button class="search-button">🔍</button></div>
   
 </header>
